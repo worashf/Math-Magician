6508e78c74f11df09d994a38b2d2e7d372c0f86d
worashf <uppert83@gmail.com>
2022-07-01 09:15:16 +0300

    access total, next and operation props

--- src/components/AnswerScreen.js
+++ src/components/AnswerScreen.js
@@ -2,10 +2,17 @@
 import React, { Component } from 'react';
 
 class AnswerScreen extends Component {
+    constructor(props){
+        super(props)
+
+    }
   render() {
     return (
       <div className="answer-screen">
-        <input placeholder="0" className="answer" />
+        <input placeholder=  {this.props.next || this.props.operation || this.props.total || '0'} className="answer" /> 
+   
+
+
       </div>
     );
   }
