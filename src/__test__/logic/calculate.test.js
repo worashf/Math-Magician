9c601c9ad5aa813c807c8f1bad94e3fe55c1d70e
worashf <uppert83@gmail.com>
2022-07-06 23:48:15 +0300

    add  plus minus button test

--- src/__test__/logic/calculate.test.js
+++ src/__test__/logic/calculate.test.js
@@ -24,4 +24,34 @@ describe('calculate function tests ', () => {
     expect(result.next).toBe(null);
     expect(result.operation).toBe(null);
   });
+  test('+/- with no input', () => {
+    const cal = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+
+    const result = calculate(cal, '+/-');
+    expect(result).toMatchObject({});
+  });
+  test('+/- with +ve input', () => {
+    const cal = {
+      total: 10,
+      next: null,
+      operation: null,
+    };
+
+    const result = calculate(cal, '+/-');
+    expect(result.total).toBe('-10');
+  });
+  test('+/- with  -ve input', () => {
+    const cal = {
+      total: -10,
+      next: null,
+      operation: null,
+    };
+
+    const result = calculate(cal, '+/-');
+    expect(result.total).toBe('10');
+  });
 });
